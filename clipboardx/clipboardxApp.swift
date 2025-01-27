5a89a3b2afbf54a6bdd12821814d092481e89cda
laz_aslam <code@lazim.one>
2025-01-27 23:26:44 +0530

    Initial Commit

---
+++ clipboardx/clipboardxApp.swift
@@ -0,0 +1,17 @@
+//
+//  clipboardxApp.swift
+//  clipboardx
+//
+//  Created by Lazim Aslam on 27/01/25.
+//
+
+import SwiftUI
+
+@main
+struct clipboardxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
